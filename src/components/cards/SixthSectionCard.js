382ac3ed1a518ff4bf7b5d9cca66cc61491ee47f
azaek <92246587+azaek@users.noreply.github.com>
2022-01-30 11:30:18 +0530

    added hover effect on tweet card

--- src/components/cards/SixthSectionCard.js
+++ src/components/cards/SixthSectionCard.js
@@ -6,14 +6,14 @@ const regex = /(?<![\w@])@([\w@]+(?:[.!][\w@]+)*)/g;
 
 const TweetCard = ({ tweetData }) => {
 
-    const bodyText =  tweetData?.desc;
+    const bodyText = tweetData?.desc;
 
     // if (bodyText.match(regex)) {
     //     console.log("matched");
     // } else {
     //     console.log("nope");
     // }
-    let m,p;
+    let m, p;
     const pre = '<span class="text-blue-400 font-semibold">';
     const suff = '</span>';
 
@@ -22,7 +22,7 @@ const TweetCard = ({ tweetData }) => {
             m = pre + url + suff;
             return m;
         })
-    } else{
+    } else {
         p = bodyText;
     }
 
@@ -32,7 +32,7 @@ const TweetCard = ({ tweetData }) => {
     //     if (m.index === regex.lastIndex) {
     //         regex.lastIndex++;
     //     }
-        
+
     //     // The result can be accessed through the `m`-variable.
     //     m.forEach((match, groupIndex) => {
     //         console.log(`Found match, group ${groupIndex}: ${match}`);
@@ -40,18 +40,37 @@ const TweetCard = ({ tweetData }) => {
     // }
 
     return (
-        <div className="flex flex-col gap-2 p-4 w-full sm:max-w-[300px] rounded-xl bg-[#1f3147] overflow-hidden">
-            <div className="flex flex-row justify-between items-center">
-                <div className="flex felx-row gap-4" >
-                    <img className=" rounded-full w-10 h-10" src={tweetData.profileImg} alt="profile-pic" />
-                    <div>
-                        <p className="text-sm">{tweetData.name}</p>
-                        <p className=" text-xs font-light opacity-[0.68]">{tweetData.username}</p>
+        <div className=" cursor-default group w-full sm:max-w-[300px] relative" >
+            <div className="flex flex-col gap-2 p-4 w-full sm:max-w-[300px] rounded-xl bg-[#1f3147] overflow-hidden opacity-0">
+                <div className="flex flex-row justify-between items-center">
+                    <div className="flex felx-row gap-4" >
+                        <img className=" rounded-full w-10 h-10" src={tweetData.profileImg} alt="profile-pic" />
+                        <div>
+                            <p className="text-sm">{tweetData.name}</p>
+                            <p className=" text-xs font-light opacity-[0.68]">{tweetData.username}</p>
+                        </div>
                     </div>
+                    <img className=" rounded-sm w-[26px] h-[26px]" src={twitter} alt="twitter-icon" />
                 </div>
-                <img className=" rounded-sm w-[26px] h-[26px]" src={twitter} alt="twitter-icon" />
+                <p className=" pb-2 text-sm font-light opacity-[0.68]" dangerouslySetInnerHTML={{ __html: `${p}` }} >{ }</p>
             </div>
-            <p className=" pb-2 text-sm font-light opacity-[0.68]" dangerouslySetInnerHTML={{ __html: `${p}` }} >{}</p>
+
+            {
+                <div className="transition-all ease-in-out group-hover:shadow-xl group-hover:z-[100] group-hover:ring-[1px] group-hover:ring-blue-500/30 group-hover:shadow-blue-700/50 absolute inset-0 duration-300 scale-100 group-hover:scale-125 flex flex-col gap-2 p-4 w-full sm:max-w-[300px] rounded-xl bg-[#1f3147] overflow-hidden">
+                    <div className="flex flex-row justify-between items-center">
+                        <div className="flex felx-row gap-4" >
+                            <img className=" rounded-full w-10 h-10" src={tweetData.profileImg} alt="profile-pic" />
+                            <div>
+                                <p className="text-sm">{tweetData.name}</p>
+                                <p className=" text-xs font-light opacity-[0.68]">{tweetData.username}</p>
+                            </div>
+                        </div>
+                        <img className=" rounded-sm w-[26px] h-[26px]" src={twitter} alt="twitter-icon" />
+                    </div>
+                    <p className=" pb-2 text-sm font-light opacity-[0.68]" dangerouslySetInnerHTML={{ __html: `${p}` }} >{ }</p>
+                </div>
+            }
+
         </div>
 
     )
